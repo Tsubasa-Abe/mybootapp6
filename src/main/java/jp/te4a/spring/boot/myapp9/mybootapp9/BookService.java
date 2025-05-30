712fab536551dff34fddf1a51d2bf0956a5485e1
Tsubasa-Abe <jcte220306@gmail.com>
2025-05-30 14:26:36 +0900

    コミットメッセージ

---
+++ src/main/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookService.java
@@ -0,0 +1,91 @@
+package jp.te4a.spring.boot.myapp9.mybootapp9;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService{
+
+    @Autowired
+    BookRepository bookRepository;
+
+    public BookForm save(BookForm bookForm){
+        BookBean bookBean = new BookBean();
+        BeanUtils.copyProperties(bookForm, bookBean);
+        bookRepository.save(bookBean);
+        return bookForm;
+    }
+
+    public BookForm update(BookForm bookForm){
+        BookBean bookBean = new BookBean();
+        BeanUtils.copyProperties(bookForm, bookBean);
+        bookRepository.save(bookBean);
+        return bookForm;
+    }
+
+    public void delete(Integer id){
+        BookBean bookBean = new BookBean();
+        bookBean.setId(id);
+        bookRepository.delete(bookBean);
+    }
+
+    public List<BookForm> findAll(){
+        List<BookBean> beanList = bookRepository.findAll();
+        List<BookForm> formList = new ArrayList<BookForm>();
+        for(BookBean bookBean: beanList){
+            BookForm bookForm = new BookForm();
+            BeanUtils.copyProperties(bookBean, bookForm);
+            formList.add(bookForm);
+        }
+        return formList;
+    }
+
+    public BookForm findOne(Integer id){
+        Optional<BookBean> optionalBook = bookRepository.findById(id);
+        BookForm bookForm = new BookForm();
+        BeanUtils.copyProperties(optionalBook.get(), bookForm);
+        return bookForm;
+    }
+}
+
+/*
+
+    public BookForm create(BookForm bookForm){
+        bookForm.setId(bookRepository.getBookId());
+        BookBean bookBean = new BookBean();
+        BeanUtils.copyProperties(bookForm,bookBean);
+        bookRepository.save(bookBean);
+        return bookForm;
+    }
+
+    public BookForm update(BookForm bookForm){
+        BookBean bookBean = new BookBean();
+        BeanUtils.copyProperties(bookForm,bookBean);
+        bookRepository.update(bookBean);
+        return bookForm;
+    }
+
+    public void delete(Integer id){ bookRepository.delete(id);}
+    public List<BookForm> findAll(){
+        List<BookBean> beanList = bookRepository.findAll();
+        List<BookForm> formList = new ArrayList<BookForm>();
+        for(BookBean bookBean: beanList){
+            BookForm bookForm = new BookForm();
+            BeanUtils.copyProperties(bookBean, bookForm);
+            formList.add(bookForm);
+        }
+        return formList;
+    }
+
+    public BookForm findOne(Integer id){
+        BookBean bookBean = bookRepository.findOne(id);
+        BookForm bookForm = new BookForm();
+        BeanUtils.copyProperties(bookBean, bookForm);
+        return bookForm;
+    }
+}*/
---
+++ src/test/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookApplication.java
@@ -0,0 +1,26 @@
+package jp.te4a.spring.boot.myapp9.mybootapp9;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+import jp.te4a.spring.boot.myapp9.mybootapp9.BookApplication;
+
+@SpringBootApplication
+public class BookApplication {
+	public static void main(String[] args){
+		SpringApplication.run(BookApplication.class,args);
+	}
+
+/*
+
+@Test
+	void contextLoads() {
+	}
+
+*/
+
+}
+	
+
