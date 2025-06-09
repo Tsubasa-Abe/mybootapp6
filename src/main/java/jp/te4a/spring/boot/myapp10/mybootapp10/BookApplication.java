bd7ed3c7be51cc87d16b24bba9272d9d367759ef
Tsubasa-Abe <jcte220306@gmail.com>
2025-06-09 11:22:47 +0900

    コミットメッセージ

--- src/main/java/jp/te4a/spring/boot/myapp10/mybootapp10/BookApplication.java
+++ src/main/java/jp/te4a/spring/boot/myapp10/mybootapp10/BookApplication.java
@@ -1,9 +1,9 @@
-package jp.te4a.spring.boot.myapp9.mybootapp9;
+package jp.te4a.spring.boot.myapp10.mybootapp10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jp.te4a.spring.boot.myapp9.mybootapp9.BookApplication;
+import jp.te4a.spring.boot.myapp10.mybootapp10.BookApplication;
 
 @SpringBootApplication
 public class BookApplication {
--- src/test/java/jp/te4a/spring/boot/myapp10/mybootapp10/BookApplication.java
+++ src/test/java/jp/te4a/spring/boot/myapp10/mybootapp10/BookApplication.java
@@ -1,11 +1,11 @@
-package jp.te4a.spring.boot.myapp9.mybootapp9;
+package jp.te4a.spring.boot.myapp10.mybootapp10;
 
 //import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.test.context.SpringBootTest;
 
-import jp.te4a.spring.boot.myapp9.mybootapp9.BookApplication;
+import jp.te4a.spring.boot.myapp10.mybootapp10.BookApplication;
 
 @SpringBootApplication
 public class BookApplication {
