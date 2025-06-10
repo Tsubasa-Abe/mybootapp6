29156d1b9a1c4e1d50b6ee3dbb0262aed245c3f3
Tsubasa-Abe <jcte220306@gmail.com>
2025-06-10 12:27:33 +0900

    コミットメッセージ

---
+++ src/main/java/jp/te4a/spring/boot/myapp11/mybootapp11/BookForm.java
@@ -0,0 +1,16 @@
+package jp.te4a.spring.boot.myapp11.mybootapp11;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+
+public class BookForm {
+    private Integer id;
+    private String title;
+    private String writer;
+    private String publisher;
+    private Integer price;
+}
